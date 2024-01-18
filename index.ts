c43ad64dbc3f15fcddec3ff16efee37c41eb71f0
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-01-18 13:46:07 +0200

    Add nodemon

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.listen(3000, () => console.log("Server listening at port 3000"));
 
 app.get("/", (req: any, res: any) => {
-  res.send("Hello World");
+  res.send("Hello World 2");
 });
 
 app.get("/healthcheck", (req: any, res: any) => {
