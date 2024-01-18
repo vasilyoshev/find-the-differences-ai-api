1b2454218f145cf251f8cde1513939a0253bcfea
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-01-18 13:18:39 +0200

    Add TS

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => console.log("Server listening at port 3000"));
+
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
+});
