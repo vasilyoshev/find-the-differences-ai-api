91e366fb463677a47d4e4dd68f70b7fed5cdc79d
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-01-18 13:25:58 +0200

    Add healthcheck endpoint

--- index.ts
+++ index.ts
@@ -7,3 +7,7 @@ app.listen(3000, () => console.log("Server listening at port 3000"));
 app.get("/", (req: any, res: any) => {
   res.send("Hello World");
 });
+
+app.get("/healthcheck", (req: any, res: any) => {
+  res.send("healthcheck");
+});
