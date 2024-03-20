862fb0ded1498d1dd8dbbaa5c15bddb5bcbd2682
Vasil Yoshev <vasil.yoshev@gmail.com>
2024-03-20 11:45:29 +0200

    FD-13: Set up ConfigModule and use env var in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // enables using .env variables in module
     HttpModule.register({
       baseURL: 'https://api.stability.ai/v1/',
       headers: {
         Accept: 'application/json',
-        authorization: 'Bearer sk-xRwOzr8D6m8M3zuDLIoYYz167iPSnfRreHSHjhHI5XimDcdP',
+        authorization: `Bearer ${process.env.STABILITY_API_KEY}`,
       },
     }),
   ],
